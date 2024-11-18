ab9618e299ee6a991e5ad5044feb5b791074e274
saurabhrane1199 <saurabhrane1199@gmail.com>
2024-11-18 00:38:08 -0500

    Fixed adshowcase issue

--- static/js/runAdAuction.js
+++ static/js/runAdAuction.js
@@ -2,28 +2,34 @@
 const sspUrl = 'https://localhost:8080';
 const dspUrl = 'https://localhost:8080';
 const auctionConfig = {
-    seller: `${sspUrl}`,
-    decisionLogicUrl: `${sspUrl}/static/js/decision-logic.js`,
-    interestGroupBuyers: [dspUrl],
-    auctionSignals: { isControversial: true },
-    sellerSignals: { key: 'value' },
-    sellerTimeout: 100,
-    perBuyerSignals: {
-      [dspUrl]: { windowInnerHeight: window.innerHeight },
-    },
-    perBuyerTimeouts: {
-      '*': 50,
-    },
+  seller: `${sspUrl}`,
+  decisionLogicUrl: `${sspUrl}/static/js/decision-logic.js`,
+  interestGroupBuyers: [dspUrl],
+  resolveToConfig: true,
+  auctionSignals: { isControversial: true },
+  sellerSignals: { key: 'value' },
+  sellerTimeout: 100,
+  perBuyerSignals: {
+    [dspUrl]: { windowInnerHeight: window.innerHeight },
+  },
+  perBuyerTimeouts: {
+    '*': 50,
+  },
 };
 
-// Run the ad auction
-navigator.runAdAuction(auctionConfig)
-.then((auctionResult) => {
-  // Display the ad
-  const iframeEl = document.getElementById('fledge-ad');
-  iframeEl.src = auctionResult;
-  console.log('Auction result', auctionResult);
-}).catch((err) => {
-    console.log('Failed to run ad auction', err);
+
+document.addEventListener("DOMContentLoaded", function () {
+  navigator.runAdAuction(auctionConfig)
+    .then((auctionResult) => {
+      // Display the ad
+      const iframeEl = document.getElementById('fledge-ad');
+      iframeEl.config = auctionResult
+      console.log(auctionResult)
+
+    }).catch((err) => {
+      console.log('Failed to run ad auction', err);
+    });
+
 });
+
 
